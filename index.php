ab514f8f4a6cd73927d9271158d85d6a87dc5e19
girro <45013197+girro@users.noreply.github.com>
2019-06-18 14:52:37 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<<?php $firstname = "Bla" ?>
+<html>
+  <head>
+    <title>Probni</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link type="text/css" rel="stylesheet" href="style.css">
+  </head>
+  <body>
+
+<div class="wrapper">
+  <div class="nav">
+    <div class="logo1">
+      LOGO LGO
+    </div>
+    <div class="logo2">
+      NAV1
+    </div>
+    <div class="logo3">
+      NAV2
+    </div>
+  </div>
+
+
+  <div class="main">
+    <div class="leftCol">
+      <form action="GET">
+   First name:<br>
+   <label for="firstname"><input type="text" name="firstname" value="<?php echo $firstname;?>"></label>
+ </form>
+    </div>
+    <div class="centerCol">
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="rightCol">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+  </div>
+
+
+
+  <div class="footer4">
+6
+  </div>
+  <div class="footer">
+3
+  </div>
+  <div class="footer2">
+4
+  </div>
+  <div class="footer3">
+5
+  </div>
+
+</div>
+
+
+
+
+
+  </body>
+</html>
